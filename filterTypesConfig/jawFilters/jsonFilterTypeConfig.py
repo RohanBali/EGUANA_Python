7725eabd95eabc284f976f3f90af659853fc81dd
Rohan Bali <roh10roh@gmail.com>
2016-12-01 11:22:40 -0500

    RohanB - added delete functionality

--- filterTypesConfig/jawFilters/jsonFilterTypeConfig.py
+++
@@ -1,8 +0,0 @@
-from filterTypesConfig.eguanaFilterTypesConfig import EguanaFilterTypesConfig
-
-class JsonFilterTypeConfig(EguanaFilterTypesConfig):
-	name = "JSON Filter" 
-	filterType = "Jaw"
-	def __init__(self):
-		EguanaFilterTypesConfig.__init__(self)  
-
