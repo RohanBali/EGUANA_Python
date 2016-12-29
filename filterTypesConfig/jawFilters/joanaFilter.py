b8dd7ac33dbe86a484df09c32b30df1860409c54
jessicaliu6251 <jiaxinjessica.liu@mail.utoronto.ca>
2016-12-29 17:17:43 -0500

    added code for 3dpe filter and fixed errors in other filters

--- filterTypesConfig/jawFilters/joanaFilter.py
+++ filterTypesConfig/jawFilters/joanaFilter.py
@@ -26,30 +26,30 @@ class JoanaFilter(EguanaFilterTypesConfig):
 		
 		if (articulatorSignalList.shape[0] == referenceSignalList.shape[0] and articulatorSignalList.shape[1] == referenceSignalList.shape[1]):
 		
-			theta = referenceSignalList.T[4];
-			phi = referenceSignalList.T[3];
+			theta = referenceSignalList.transpose[4];
+			phi = referenceSignalList.transpose[3];
 			
 			"""x - xj & y - yj & z - zj"""
-			diffX = articulatorSignalList.T[0] - referenceSignalList.T[0];
-			diffY = articulatorSignalList.T[1] - referenceSignalList.T[1];
-			diffZ = articulatorSignalList.T[2] - referenceSignalList.T[2];
+			diffX = articulatorSignalList.transpose[0] - referenceSignalList.transpose[0];
+			diffY = articulatorSignalList.transpose[1] - referenceSignalList.transpose[1];
+			diffZ = articulatorSignalList.transpose[2] - referenceSignalList.transpose[2];
 			
 			"""extract LH & RH position vectors"""
-			LH_Vector = referenceSignalList[[:,5], [:,6], [:,7]];
-			RH_Vector = referenceSignalList[[:,8], [:,9], [:,10]];
+			LH_Vector = np.column_stack (referenceSignalList[:,5], referenceSignalList[:,6], referenceSignalList[:,7])
+			RH_Vector = np.column_stack (referenceSignalList[:,8], referenceSignalList[:,9], referenceSignalList[:,10])
 
 			for i in range (len(referenceSignalList[0])):
 			
 				"""define new x,y,z axes """
-				transX_axis = (cos(theta[i])*cos(phi[i]), cos(theta[i])*sin(phi[i]), sin(theta[i]));   	'''converting spherical to cartisian'''				
+				transX_axis = (cos(theta[0][i])*cos(phi[0][i]), cos(theta[0][i])*sin(phi[0][i]), sin(theta[0][i]));   	'''converting spherical to cartisian'''				
 				transY_axis = (RH_Vector[i] - LH_Vector[i]); 										   	'''position of RH - position of LF'''
 				transY_axis = transY_axis/np.linalg.norm(transY_axis);									'''normalized => vector t'''
 				transZ_axis = np.cross(transX_axis, transY_axis); 										'''z-axis => x-axis cross y-axis'''
 				
 				transMatrix = (transX_axis, transY_axis, transZ_axis);
-				diffVec = (diffX[i], diffY[i], diffZ[i]);
+				diffVec = (diffX[0][i], diffY[0][i], diffZ[0][i]);
 				
-				correctedList[i] = transMatrix * diffVec;
+				correctedList[i] = transMatrix * diffVec.transpose();
 			
 			return correctedList;
 		else: 
