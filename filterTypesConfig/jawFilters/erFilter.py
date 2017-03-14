be965d3cec0daedb061e3c42d550f6e81653db6c
jessicaliu6251 <jiaxinjessica.liu@mail.utoronto.ca>
2017-03-14 15:05:09 -0400

    fixed ER filter. https://trello.com/c/5VkW8Jvj

--- filterTypesConfig/jawFilters/erFilter.py
+++ filterTypesConfig/jawFilters/erFilter.py
@@ -1,6 +1,8 @@
 from filterTypesConfig.eguanaFilterTypesConfig import EguanaFilterTypesConfig
 import numpy as np
-from matplotlib.mlab import PCA
+#from matplotlib.mlab import PCA
+from sklearn.decomposition import PCA
+from filterTypesConfig.jawFilters.joanaFilter import JoanaFilter
 import math
 
 class ErFilter(EguanaFilterTypesConfig):
@@ -27,14 +29,37 @@ class ErFilter(EguanaFilterTypesConfig):
 			"""
 			take out the Y component to calculate PCA
 			"""
+			# LH = referenceSignalList[:,[3,4,5]]
+			# '''print (LH_Vector[1])'''
+			# RH = referenceSignalList[:,[6,7,8]]
+			# FH = referenceSignalList[:,[9,10,11]]
+			# jaw = referenceSignalList[:,[0,1,2]]
+			
+			# hc_Jaw = JoanaFilter.filter(jaw, )
+			
+			
+			dataList = np.column_stack((referenceSignalList[:,0],referenceSignalList[:,2]))	
 			ref_list_ignore_Y = np.delete(referenceSignalList,1,1);
-			dataList = np.array (ref_list_ignore_Y);
+			dataList = np.array (dataList);
+			#print (dataList)
 			
 			"""
 			do PCA on dataList, then calculate alpha using the experimentally determined correlation constant
 			"""
+			p = PCA (n_components=2)
+			myPCA = p.fit_transform(dataList)[:,0]
 			
-			alpha = ANGLE_PC_CORRELATION_CONST * (PCA (dataList)).Y[:,0];
+			#check correlation
+			if (np.corrcoef(dataList[:,1], myPCA)[0,1] < 0):
+				myPCA = myPCA * (-1)
+				
+				
+			max = np.max(myPCA)
+			myPCA = myPCA - max
+			alpha = ANGLE_PC_CORRELATION_CONST *myPCA;
+			
+			#subtract the maximum from PCA.
+			#print (p.fit_transform(dataList))
 			
 			""" transformation of articulatorSignalList to correctedList
 			"""
